99aed0f437a36ff0896b5b5e0dce561acda986c8
fujita-seiryu <fujita.seiryu@sonaeru-project.jp>
2026-08-17 09:10:15 +0900

    Add files via upload

---
+++ voice-training/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'vt-pitch-trainer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネットワーク優先: 常に最新版を取得し、オフライン時のみキャッシュにフォールバック。
+// 合言葉ロックや script.js の修正が即座に反映されるようにするため。
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
